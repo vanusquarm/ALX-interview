4d665cbd06dcec2a00aaa9938691bf9827bd019b
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2022-11-08 15:43:46 +0000

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,45 +1,34 @@
-#eg. 5C2
-#super = 5;
-#sub = 2;
-
-def nCr(sup,sub):
-    nom = 1
-    denom = 1
-    while(sub > 0):
-        nom *= sup
-        denom *= sub
-	    
-        sub -= 1
-        sup -= 1
-        
-    return int(nom/denom)
-
-
-
-#eg. pascal coefficient of 2 is [1 2 1]
-
-def pascal_coeff(power):
-    coeffs = []
-    for i in range(power+1):
-        coeffs.append(nCr(power,i))
-	
-    return coeffs
-	
-
-
 '''
-0C0           1
-1C0 1C1      1  1 
-2C0 2C1 2C0 1  2  1
+calculates and draws the graph for a pascal triangle using the combination approach
 '''
 
 def pascal_triangle(height):
     triangle = []
-
+    # check against neg height
     if height <= 0:
         return [[]]
     
-    for i in range(height+1):
-        triangle.append(pascal_coeff(i))
+    for h in range(height+1):
+        coeffs = []
+        
+        # calculate pascal coeffs for each degree
+        power = h
+        for i in range(power + 1):
+            sub = i
+            sup = power
+            nom = 1
+            denom = 1
+			
+            # calculate nCr
+            while( sub> 0):
+                nom *= sup
+                denom *= sub
+
+                sub -= 1
+                sup -= 1
+
+            coeffs.append(int(nom/denom))
+
+        triangle.append(coeffs)
         
     return triangle
